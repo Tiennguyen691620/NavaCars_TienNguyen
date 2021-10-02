9029ba8a22aa7826f205bda9c671508a612ec5f9
Tiennguyen691620 <71544985+Tiennguyen691620@users.noreply.github.com>
2021-10-02 15:02:58 +0700

    first

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+$( document ).ready(function() { 
+    window.addEventListener('scroll', function(){
+        let header = this.document.querySelector('header');
+        let windowPosition = window.scrollY > 73;
+        header.classList.toggle('scrolling-active', windowPosition);
+    })
+
+    var header = document.getElementById('header');
+            var mobileMenu = document.getElementById('mobile-menu');
+            var headerHeight = header.clientHeight
+            
+            mobileMenu.onclick = function(){
+                var isClose = header.clientHeight === headerHeight;
+                if(isClose){
+                    header.style.height='auto';
+                }else {
+                    header.style.height = null;
+                }
+            }
+
+});
